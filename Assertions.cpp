0982bdde7afc259d8e34a0132dec7e451407abce
David Delarosa <xdavidel@gmail.com>
2017-10-22 15:11:49 +0300

    Add assertions

--- Assertions.cpp
+++ Assertions.cpp
@@ -7,6 +7,8 @@
 // Concatenates two strings. str1 and str2 must not be null.
 char *concatenate(char *str1, char *str2)
 {
+	assert(str1 != NULL);
+	assert(str2 != NULL);
 	size_t size = strlen(str1) + strlen(str2) + 1;
 	char *result = new char[size];
 
@@ -20,6 +22,8 @@ typedef std::pair<double, double> Roots;
 
 Roots getQuadraticEquationRoots(double a, double b, double c)
 {
+	assert(a != 0);
+	assert(b * b - 4 * a * c >= 0);
 	double discriminant = b * b - 4 * a * c;
 	double discriminantRoot = sqrt(discriminant);
 
@@ -33,6 +37,7 @@ Roots getQuadraticEquationRoots(double a, double b, double c)
 
 int numberAt(int array[], size_t arraySize, int index)
 {
+	assert(index <= arraySize - 1 && index >= 0);
 	return array[index];
 }
 
